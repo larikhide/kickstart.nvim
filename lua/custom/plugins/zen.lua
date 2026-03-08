fe2cf058ca151b62d77304e9b4feeb6b7f0fcaf9
Your Name <petrakliy@gmail.com>
2026-03-08 22:51:42 +0200

    change default zen mode width

--- lua/custom/plugins/zen.lua
+++ lua/custom/plugins/zen.lua
@@ -4,6 +4,9 @@ return {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
+		window = {
+			width = 90,
+		},
 	},
 	keys = {
 		{ "<leader>zm", "<cmd>ZenMode<cr>", desc = "Toggle Zen Mode" },
