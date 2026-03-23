486f6a4bbfbffe42f7d13506eb7512f33f2b7799
Your Name <petrakliy@gmail.com>
2026-03-24 01:31:13 +0200

    change default colorscheme

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -1,12 +1,46 @@
 return {
 
 	{
-		"neanias/everforest-nvim",
+		"folke/tokyonight.nvim",
+		-- priority = 1000, -- Make sure to load this before all the other start plugins.
+		config = function()
+			---@diagnostic disable-next-line: missing-fields
+			require("tokyonight").setup({
+				styles = {
+					comments = { italic = false }, -- Disable italics in comments
+				},
+			})
+
+			-- Load the colorscheme here.
+			-- Like many other themes, this one has different styles, and you could load
+			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+			-- vim.cmd.colorscheme("tokyonight-night")
+		end,
 	},
 
 	{
-		"rose-pine/neovim",
-		name = "rose-pine",
+		"neanias/everforest-nvim",
+		lazy = false,
+		-- priority = 1000, -- make sure to load this before all the other start plugins
+		-- Optional; default configuration will be used if setup isn't called.
+		config = function()
+			require("everforest").setup({
+				-- Your config here
+			})
+			-- vim.cmd.colorscheme("everforest")
+		end,
+	},
+
+	{
+		"maxmx03/solarized.nvim",
+		lazy = false,
+		priority = 1000,
+		---@type solarized.config
+		opts = {},
+		config = function(_, opts)
+			require("solarized").setup(opts)
+			vim.cmd.colorscheme("solarized")
+		end,
 	},
 
 	{
